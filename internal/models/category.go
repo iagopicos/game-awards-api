929f0ddb7c52cd1920245ea4ccf8f97897becd0c
Iago Fernandez Picos <iagofernandez94@gmail.com>
2025-11-27 23:34:34 +0100

    game model updated

--- internal/models/category.go
+++ internal/models/category.go
@@ -1,3 +1,5 @@
 package models
 
-type Category struct{}
+type Category struct {
+	CategoryID uint
+}
--- internal/models/game.go
+++ internal/models/game.go
@@ -1,3 +1,23 @@
 package models
 
-type Game struct{}
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Game struct {
+	GameID      uuid.UUID    `gorm:"type:uuid;primaryKey;default:uuid_generate_4()" json:"game_id"`
+	Title       string       `gorm:"type:varchar(255);not null" json:"title"`
+	ReleaseDate *time.Time   `gorm:"type:date" json:"release_date"`
+	ImageURL    string       `gorm:"type:varchar(500)" json:"image_url"`
+	Nominations []Nomination `gorm:"foreignKey:GameID" json:"nominations,omitempty"`
+	Awards      []Award      `gorm:"foreignKey:GameWinnerID" json:"awards,omitempty"`
+	Predictions []Prediction `gorm:"foreignKey:GameIDPredicted" json:"predictions,omitempty"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+}
+
+func (Game) TableName() string {
+	return "games"
+}
