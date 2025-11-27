d7b7924bd0176c6d39bc5f48d43ad3fe8b62829f
Iago Fernandez Picos <iagofernandez94@gmail.com>
2025-11-27 21:44:49 +0100

    Add database config to the database, init go project

---
+++ config/config.go
@@ -0,0 +1,88 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Database DatabaseConfig
+	Server   ServerConfig
+}
+
+type DatabaseConfig struct {
+	Host            string
+	Port            string
+	User            string
+	Password        string
+	DBName          string
+	SSLMode         string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+}
+
+type ServerConfig struct {
+	Port string
+}
+
+func Load() (*Config, error) {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	return &Config{
+		Database: DatabaseConfig{
+			Host:            getEnv("DB_HOST", "localhost"),
+			Port:            getEnv("DB_PORT", "5432"),
+			User:            getEnv("DB_USER", "postgres"),
+			Password:        getEnv("DB_PASSWORD", ""),
+			DBName:          getEnv("DB_NAME", "myproject_db"),
+			SSLMode:         getEnv("DB_SSLMODE", "disable"),
+			MaxIdleConns:    getEnvInt("DB_MAX_IDLE_CONNS", 10),
+			MaxOpenConns:    getEnvInt("DB_MAX_OPEN_CONNS", 100),
+			ConnMaxLifetime: getEnvDuration("DB_CONN_MAX_LIFETIME", time.Hour),
+		},
+		Server: ServerConfig{
+			Port: getEnv("SERVER_PORT", "8080"),
+		},
+	}, nil
+}
+
+func (c *DatabaseConfig) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode,
+	)
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvInt(key string, defaultValue int) int {
+	if value := os.Getenv(key); value != "" {
+		if intVal, err := strconv.Atoi(value); err == nil {
+			return intVal
+		}
+	}
+	return defaultValue
+}
+
+func getEnvDuration(key string, defaultValue time.Duration) time.Duration {
+	if value := os.Getenv(key); value != "" {
+		if duration, err := time.ParseDuration(value); err == nil {
+			return duration
+		}
+	}
+	return defaultValue
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iagopicos/game-awards-api
+
+go 1.25.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
